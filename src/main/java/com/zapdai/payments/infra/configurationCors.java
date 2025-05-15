167584dc61744b23b400d7978d87bc0e49ad80bf
elivandro <elivandro78@gmail.com>
2025-05-15 15:03:26 -0300

    corrigindo erro de cors

---
+++ src/main/java/com/zapdai/payments/infra/configurationCors.java
@@ -0,0 +1,24 @@
+package com.zapdai.payments.infra;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class configurationCors {
+    @Bean
+    public WebMvcConfigurer corsConfigure(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                ;
+            }
+        };
+    }
+}
